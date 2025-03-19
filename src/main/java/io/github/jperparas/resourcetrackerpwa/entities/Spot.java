af16a0f1a296e8ec5b7ecb6f4dbfc644329cdfcc
Jperparas <j.s.perparas@gmail.com>
2025-03-19 16:04:23 -0700

    added gpu and pca sets with one to many annotations to Spot entity

--- src/main/java/io/github/jperparas/resourcetrackerpwa/entities/Spot.java
+++ src/main/java/io/github/jperparas/resourcetrackerpwa/entities/Spot.java
@@ -2,6 +2,8 @@ package io.github.jperparas.resourcetrackerpwa.entities;
 
 import jakarta.persistence.*;
 
+import java.util.Set;
+
 @Entity
 @Table(name ="spots")
 public class Spot {
@@ -13,4 +15,11 @@ public class Spot {
     String name;
     @Column(name="has_chocks")
     boolean hasChocks;
+
+    @OneToMany(mappedBy = "spot")
+    Set<Gpu> gpus;
+
+    @OneToMany(mappedBy = "spot")
+    Set<Pca> pcas;
+
 }
